85430047f50c9093c660ba7f026e32fde300fdb2
joffroy59 <jerome.offroy@gmail.com>
2015-04-27 21:15:44 +0200

    add parameter to touch goal

--- src/main/java/org/offroy/maven/plugin/openfile/MyMojo.java
+++ src/main/java/org/offroy/maven/plugin/openfile/MyMojo.java
@@ -16,66 +16,56 @@ package org.offroy.maven.plugin.openfile;
  * limitations under the License.
  */
 
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-
-import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
 /**
  * Goal which touches a timestamp file.
- *
+ * 
  * @deprecated Don't use!
  */
-@Mojo( name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
-public class MyMojo
-    extends AbstractMojo
-{
+@Mojo(name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
+public class MyMojo extends AbstractMojo {
     /**
      * Location of the file.
      */
-    @Parameter( defaultValue = "${project.build.directory}", property = "outputDir", required = true )
+    @Parameter(defaultValue = "${project.build.directory}", property = "touch.outputDir", required = true)
     private File outputDirectory;
 
-    public void execute()
-        throws MojoExecutionException
-    {
+    /**
+     * Name of the file.
+     */
+    @Parameter(defaultValue = "touch.txt", property = "touch.fileName", required = true)
+    private String fileName;
+
+    public void execute() throws MojoExecutionException {
         File f = outputDirectory;
 
-        if ( !f.exists() )
-        {
+        if (!f.exists()) {
             f.mkdirs();
         }
 
-        File touch = new File( f, "touch.txt" );
+        File touch = new File(f, fileName);
 
         FileWriter w = null;
-        try
-        {
-            w = new FileWriter( touch );
+        try {
+            w = new FileWriter(touch);
 
-            w.write( "touch.txt" );
-        }
-        catch ( IOException e )
-        {
-            throw new MojoExecutionException( "Error creating file " + touch, e );
-        }
-        finally
-        {
-            if ( w != null )
-            {
-                try
-                {
+            w.write(fileName);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error creating file " + touch, e);
+        } finally {
+            if (w != null) {
+                try {
                     w.close();
-                }
-                catch ( IOException e )
-                {
+                } catch (IOException e) {
                     // ignore
                 }
             }
